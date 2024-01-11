fa6e4cb927a54f2cc1447d84bbb4e0079e799684
Yu Chong <97482664+Liao-Yu-Chong@users.noreply.github.com>
2024-01-11 22:56:15 +0800

    feat: 建立公告清單

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
@@ -7,6 +6,14 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  data = [
+    { title: "公告標題 01", createdAt: 1703496488777, content: "公告01的內文" },
+    { title: "公告標題 02", createdAt: 1703496488777, content: "公告02的內文" },
+    { title: "公告標題 03", createdAt: 1703496488777, content: "公告03的內文" },
+    { title: "公告標題 04", createdAt: 1703496488777, content: "公告04的內文" },
+    { title: "公告標題 05", createdAt: 1703496488777, content: "公告05的內文" },
+  ];
+
+  constructor() { }
 
 }
